1dec7d06cdf35fd8252602098cd08f183d5489cc
Flame25 <noobguideforyou@gmail.com>
2021-03-31 21:26:23 +0700

    inital commit

---
+++ WPFTreeView/HeaderToImage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;  
+
+namespace WPFTreeView
+{
+    [ValueConversion(typeof(string),typeof(BitmapImage))] 
+    class HeaderToImage : IValueConverter
+    {
+        public static HeaderToImage Instance = new HeaderToImage(); 
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var path = (string)value;
+            if (path == null)
+                return null;
+            var image = "image/file.png";
+            var name = MainWindow.GetFolderName(path); 
+            if(string.IsNullOrEmpty(name))
+            {
+                image = "image/drive.png"; 
+            }
+            else if (new FileInfo(path).Attributes.HasFlag(FileAttributes.Directory))
+            {
+                image = "image/folder-closed.png"; 
+            }
+
+            return new BitmapImage(new Uri($"pack://application:,,,/{image}")); 
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WPFTreeView/MainWindow.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.IO;
+
+namespace WPFTreeView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Default Constructor
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region On Loaded
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (var drive in Directory.GetLogicalDrives()) 
+            {
+                var item = new TreeViewItem()
+                {
+                    Header = drive,
+                    Tag = drive
+                };
+                item.Items.Add(null);
+                item.Expanded += Folder_Expanded; 
+                FolderView.Items.Add(item); 
+            }
+        }
+        #endregion
+
+        #region Folder Expanded
+        private void Folder_Expanded(Object sender, RoutedEventArgs e)
+        {
+            #region Inital Check
+            var item = (TreeViewItem)sender;
+            if (item.Items.Count != 1 || item.Items[0] != null)
+                return;
+
+            item.Items.Clear();
+            
+            var fullPath = (string)item.Tag;
+            #endregion
+
+            #region Get Directories
+            var dirs = new List<string>();
+           
+            try
+            {
+                var dir = Directory.GetDirectories(fullPath); 
+
+                if(dir.Length > 0)
+                {
+                    dirs.AddRange(dir); 
+                }
+            }
+            catch { }
+            
+            dirs.ForEach(dirPath =>
+            {
+                var subItem = new TreeViewItem()
+                {
+                    Header = GetFolderName(dirPath),
+                    Tag = dirPath
+                };
+
+
+                subItem.Items.Add(null); 
+                subItem.Expanded += Folder_Expanded;
+                item.Items.Add(subItem); 
+            });
+
+            #endregion
+
+            #region Get Files
+            var fs = new List<string>();
+
+            try
+            {
+                var files = Directory.GetFiles(fullPath);
+
+                if (files.Length > 0)
+                {
+                    fs.AddRange(files);
+                }
+            }
+            catch { }
+
+            fs.ForEach(filePath =>
+            {
+                var subItem = new TreeViewItem()
+                {
+                    Header = GetFolderName(filePath),
+                    Tag = filePath
+                };
+                subItem.Expanded += Folder_Expanded;
+                item.Items.Add(subItem);
+            });
+            #endregion
+        }
+        #endregion
+
+        #region Helpers
+        public static string GetFolderName(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return string.Empty;
+            }
+
+            var normalized = s.Replace('/', '\\');
+
+            var lastIndex = normalized.LastIndexOf("\\");
+
+            if (lastIndex < 0)
+                return s;
+
+            return normalized.Substring(lastIndex + 1);
+        }
+
+        #endregion
+    }
+}
